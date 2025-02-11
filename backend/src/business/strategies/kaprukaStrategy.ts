5920fc8992a6ec0fda41075f8515cdac06c520d1
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 22:13:04 +0530

    add the parser to the kapruka strategy

--- backend/src/business/strategies/kaprukaStrategy.ts
+++ backend/src/business/strategies/kaprukaStrategy.ts
@@ -1,6 +1,23 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class KaprukaStrategy extends ILotteryStrategy {
+
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Kapruka";
+        lottery.drawNo = tokens[1];
+        lottery.date = this.formatDate(tokens[2]);
+        lottery.barCode = tokens[3];
+        lottery.numbers = tokens.slice(4, 8);
+        lottery.symbole = tokens[8];
+        lottery.symbole1 = tokens[9];
+        lottery.specialSymboles = tokens.slice(10);
+
+        return lottery;
+    }
+
     toString(): string {
         return "kapruka";
     }
