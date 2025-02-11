9883a0c56d05b0f83b6fc851826bf9b104edaa3a
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 22:02:44 +0530

    add the parser to the jaya strategy

--- backend/src/business/strategies/jayaStrategy.ts
+++ backend/src/business/strategies/jayaStrategy.ts
@@ -1,6 +1,20 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class JayaStrategy extends ILotteryStrategy {
+
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Jaya";
+        lottery.drawNo = tokens[2];
+        lottery.date = this.formatDate(tokens[3]);
+        lottery.barCode = tokens[4];
+        lottery.symbole = tokens[5];
+        lottery.numbers = tokens.slice(6, 10);
+        lottery.specialSymboles = tokens.slice(10);
+        return lottery;
+    }
     toString(): string {
         return "jaya"
     }
@@ -8,6 +22,12 @@ export default class JayaStrategy extends ILotteryStrategy {
     async checkResult(tokens: string[]): Promise<number> {
         console.log(tokens);
         return 10;
+    }
+
+    formatDate(dateString: string): Date {
+        let [day, month, year] = dateString.split(".");
+        let formattedDate = `${year}-${month}-${day}`;
+        return new Date(formattedDate);
     }
 
 }
