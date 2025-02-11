6a52c7a5e2ccdd93ad6dbf8432840f0757df8a60
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 19:15:00 +0530

    add the parser to the dhana nidhayana strategy

--- backend/src/business/strategies/dhanaNidhanayaStrategy.ts
+++ backend/src/business/strategies/dhanaNidhanayaStrategy.ts
@@ -1,6 +1,20 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class DhanaNidhanayaStrategy extends ILotteryStrategy {
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Dhana Nidhanaya";
+        lottery.drawNo = tokens[2];
+        lottery.symbole = tokens[3];
+        lottery.numbers = tokens.slice(4, 8);
+        lottery.specialSymboles = tokens.slice(8, tokens.length - 3);
+        lottery.barCode = tokens[tokens.length - 2];
+        lottery.specialSymboles.push(tokens[tokens.length - 1]);
+        lottery.date = this.formatDate(tokens[tokens.length - 3]);
+        return lottery;
+    }
     toString(): string {
         return "Dhana nidhanaya";
     }
@@ -8,6 +22,12 @@ export default class DhanaNidhanayaStrategy extends ILotteryStrategy {
     async checkResult(tokens: string[]): Promise<number> {
         console.log(tokens);
         return 10;
+    }
+
+    private formatDate(dateString: string): Date {
+        // convert the & signs in the date string to - signs
+        const formattedDate = dateString.replace(/&/g, "-");
+        return new Date(formattedDate);
     }
 
 }
