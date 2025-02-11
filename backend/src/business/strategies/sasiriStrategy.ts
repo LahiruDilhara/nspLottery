9d05d5b19944e07f513178bfc91d8549fef4b515
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 21:43:52 +0530

    add the parser to the sasiri strategy

--- backend/src/business/strategies/sasiriStrategy.ts
+++ backend/src/business/strategies/sasiriStrategy.ts
@@ -1,6 +1,22 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class SasiriStrategy extends ILotteryStrategy {
+
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Sasiri";
+        lottery.drawNo = tokens[0];
+        lottery.date = this.formatDate(tokens[1]);
+        lottery.barCode = tokens[2];
+        lottery.numbers = tokens.slice(3, 6);
+        lottery.specialSymboles = tokens.slice(6);
+
+        return lottery;
+    }
+
+
     toString(): string {
         return "sasiri";
     }
@@ -8,6 +24,12 @@ export default class SasiriStrategy extends ILotteryStrategy {
     async checkResult(tokens: string[]): Promise<number> {
         console.log(tokens);
         return 10;
+    }
+
+    formatDate(dateString: string): Date {
+        let [year, month, day] = dateString.split(".");
+        let formattedDate = `${year}-${month}-${day}`;
+        return new Date(formattedDate);
     }
 
 }
