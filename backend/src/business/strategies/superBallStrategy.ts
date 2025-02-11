12f07424bd5a8c32dd110bba6e0f79a384209cfe
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 21:48:29 +0530

    add the parser to the super ball strategy

--- backend/src/business/strategies/superBallStrategy.ts
+++ backend/src/business/strategies/superBallStrategy.ts
@@ -1,6 +1,23 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class superBallStrategy extends ILotteryStrategy {
+
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Super Ball";
+        lottery.drawNo = tokens[2];
+        lottery.date = this.formatDate(tokens[3]);
+        lottery.barCode = tokens[4];
+        lottery.numbers = tokens.slice(5, 9);
+        lottery.symbole = tokens[9];
+        lottery.specialSymboles = tokens.slice(10);
+
+        return lottery;
+    }
+
+
     toString(): string {
         return "super ball";
     }
