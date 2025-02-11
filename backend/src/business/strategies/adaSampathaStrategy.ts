e8ece84483eb80e28463bd3111db41e5be09b49a
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 21:58:07 +0530

    add the parser to the ada sampatha strategy

--- backend/src/business/strategies/adaSampathaStrategy.ts
+++ backend/src/business/strategies/adaSampathaStrategy.ts
@@ -1,6 +1,21 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class AdaSampathaStrategy extends ILotteryStrategy {
+
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Ada Sampatha";
+        lottery.drawNo = tokens[2];
+        lottery.date = this.formatDate(tokens[3]);
+        lottery.barCode = tokens[4];
+        lottery.symbole = tokens[5];
+        lottery.numbers = tokens.slice(6, 9);
+        lottery.specialSymboles = tokens.slice(9);
+
+        return lottery;
+    }
 
     toString(): string {
         return "Ada Sampatha";
@@ -11,4 +26,9 @@ export default class AdaSampathaStrategy extends ILotteryStrategy {
         return 10;
     }
 
+    formatDate(dateString: string): Date {
+        let [day, month, year] = dateString.split(".");
+        let formattedDate = `${year}-${month}-${day}`;
+        return new Date(formattedDate);
+    }
 }
