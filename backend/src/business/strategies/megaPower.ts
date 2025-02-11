46dee3586f0ae3f17deed4bee0e7dee0c8b3e886
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 22:23:22 +0530

    add the parser to the mega power strategy

--- backend/src/business/strategies/megaPower.ts
+++ backend/src/business/strategies/megaPower.ts
@@ -1,6 +1,21 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class MegaPowerStrategy extends ILotteryStrategy {
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Mega Power";
+        lottery.drawNo = tokens[2];
+        lottery.symbole = tokens[3];
+        lottery.symbole1 = tokens[4];
+        lottery.numbers = tokens.slice(5, 9);
+        lottery.date = this.formatDate(tokens[9]);
+        lottery.barCode = tokens[10];
+        lottery.specialSymboles = tokens.slice(11);
+
+        return lottery;
+    }
     toString(): string {
         return "mega power";
     }
