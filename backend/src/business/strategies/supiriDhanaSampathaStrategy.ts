71d286174b7bed02fb6940098f7bf53e28f968d0
galahirudilhara <galahirudilhara@gmail.com>
2025-02-11 22:31:46 +0530

    add the parser to the supiri dhana sapmatha strategy

--- backend/src/business/strategies/supiriDhanaSampathaStrategy.ts
+++ backend/src/business/strategies/supiriDhanaSampathaStrategy.ts
@@ -1,6 +1,21 @@
+import LotteryEnitity from "../entities/LotteryEntity";
 import ILotteryStrategy from "../interfaces/ILotteryStrategy";
 
 export default class SupiriDhanaSampathaStrategy extends ILotteryStrategy {
+
+    parseFromQRStringTokens(tokens: string[]): LotteryEnitity {
+        const lottery = new LotteryEnitity();
+
+        lottery.name = "Supiri Dhana Sampatha";
+        lottery.drawNo = tokens[0];
+        lottery.date = this.formatDate(tokens[1]);
+        lottery.barCode = tokens[2];
+        lottery.symbole = tokens[3];
+        lottery.numbers = tokens.slice(4, 10);
+        lottery.specialSymboles = tokens.slice(10);
+
+        return lottery;
+    }
     toString(): string {
         return "supiri dhana sampatha";
     }
@@ -10,4 +25,9 @@ export default class SupiriDhanaSampathaStrategy extends ILotteryStrategy {
         return 10;
     }
 
+    formatDate(dateString: string): Date {
+        let [year, month, day] = dateString.split(".");
+        let formattedDate = `${year}-${month}-${day}`;
+        return new Date(formattedDate);
+    }
 }
