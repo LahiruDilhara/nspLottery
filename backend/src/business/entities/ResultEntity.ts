79cd1399caec7867bf9e10b71d6366bc7bd95ae4
galahirudilhara <galahirudilhara@gmail.com>
2025-02-17 14:05:49 +0530

    remove the unused entity

--- backend/src/business/entities/ResultEntity.ts
+++
@@ -1,7 +0,0 @@
-export default class ResultEntity {
-    mainResult!: number;
-    matchedMainNumbers!: [string, boolean];
-    symboleMatched: boolean = false;
-    symbole1Matched: boolean = false;
-    matchedSpecialSymboles!: [string, boolean];
-}
