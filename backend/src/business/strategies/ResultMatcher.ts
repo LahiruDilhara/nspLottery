5e79f51b7a488d6a808bfaa9e5ce9d5edd0e913a
galahirudilhara <galahirudilhara@gmail.com>
2025-03-08 12:52:43 +0530

    add the tests for the ResultMatcher and created the OneToMany, ManyToOne and ManyToMany match cases for special sybmoles. also add the testes for testing those methods

--- backend/src/business/strategies/ResultMatcher.ts
+++ backend/src/business/strategies/ResultMatcher.ts
@@ -1,45 +1,27 @@
-import { matches } from "lodash";
 import { LotterySpecialSymbole, MatchSpecialSymbole, NumberMatch, SpecialSymbole, SymboleMatch } from "../types/types";
 
 export default class ResultMatcher {
 
-    checkSpecialSymboles(wonSymboles: SpecialSymbole[], lotterySymboles: LotterySpecialSymbole[]): MatchSpecialSymbole[] {
-        let matchList: MatchSpecialSymbole[] = [];
+    matchAllInOrderDescrete(wonSymboles: string[], lotterySymboles: string[]): SymboleMatch {
+        let matches: { symbole: string, matched: boolean }[] = [];
+        let total: number = 0;
+        let matchCount: number = 0;
 
-        lotterySymboles.forEach((lotterySymbole) => {
-            // find the result symbole that match the category
-            let resultSymbole = wonSymboles.find((resultSymbole) => resultSymbole.category === lotterySymbole.category);
-
-            if (resultSymbole == undefined || resultSymbole == null) return;
-
-            // if that category is OneToOne format, call the OneToOne method to calculate the machings
-            if (resultSymbole.method === "OneToOne") {
-                matchList.push(this.oneToOneMatch(resultSymbole, lotterySymbole));
+        lotterySymboles.forEach((lotterySymbole, index) => {
+            if (wonSymboles[index] === lotterySymbole) {
+                matches.push({ symbole: lotterySymbole, matched: true });
+                matchCount++;
+            } else {
+                matches.push({ symbole: lotterySymbole, matched: false });
             }
-        });
+            total++;
+        })
 
-        return matchList;
-    }
-
-    oneToOneMatch(wonSymbole: SpecialSymbole, lotterySymbole: LotterySpecialSymbole): MatchSpecialSymbole {
-        let match: MatchSpecialSymbole = {
-            category: wonSymbole.category,
-            symboles: [],
-            gift: wonSymbole.gift,
-            matched: false
-        };
-
-        // match the symbole
-        if (lotterySymbole.symboles[0] === wonSymbole.results[0]) {
-            match.symboles.push({ matched: true, symbole: lotterySymbole.symboles[0] });
-            match.matched = true;
+        return {
+            matchCount: matchCount,
+            totalCount: total,
+            matchStatus: matches
         }
-        else {
-            match.symboles.push({ matched: false, symbole: lotterySymbole.symboles[0] })
-            match.matched = false;
-        }
-
-        return match;
     }
 
     matchAllAnyOrderDescrete(wonSymboles: string[], lotterySymboles: string[]): SymboleMatch {
@@ -63,25 +45,115 @@ export default class ResultMatcher {
             matchStatus: matches
         }
     }
-    matchAllInOrderDescrete(wonSymboles: string[], lotterySymboles: string[]): SymboleMatch {
-        let matches: { symbole: string, matched: boolean }[] = [];
-        let total: number = 0;
-        let matchCount: number = 0;
 
-        lotterySymboles.forEach((lotterySymbole, index) => {
-            if (wonSymboles[index] === lotterySymbole) {
-                matches.push({ symbole: lotterySymbole, matched: true });
-                matchCount++;
-            } else {
-                matches.push({ symbole: lotterySymbole, matched: false });
+    checkSpecialSymboles(wonSymboles: SpecialSymbole[], lotterySymboles: LotterySpecialSymbole[]): MatchSpecialSymbole[] {
+        let matchList: MatchSpecialSymbole[] = [];
+
+        lotterySymboles.forEach((lotterySymbole) => {
+            // find the result symbole that match the category
+            let resultSymbole = wonSymboles.find((resultSymbole) => resultSymbole.category === lotterySymbole.category);
+
+            if (resultSymbole == undefined || resultSymbole == null) return;
+
+            // if that category is OneToOne format, call the OneToOne method to calculate the machings
+            if (resultSymbole.method === "OneToOne") {
+                matchList.push(this.oneToOneMatch(resultSymbole, lotterySymbole));
             }
-            total++;
+            else if (resultSymbole.method === "OneToMany") {
+                matchList.push(this.oneToManyMatch(resultSymbole, lotterySymbole))
+            }
+            else if (resultSymbole.method === "ManyToOne") {
+                matchList.push(this.manyToOneMatch(resultSymbole, lotterySymbole))
+            }
+            else if (resultSymbole.method === "ManyToMany") {
+                matchList.push(this.manyToManyMatch(resultSymbole, lotterySymbole))
+            }
+        });
+
+        return matchList;
+    }
+
+    oneToOneMatch(wonSymbole: SpecialSymbole, lotterySymbole: LotterySpecialSymbole): MatchSpecialSymbole {
+        let match: MatchSpecialSymbole = {
+            category: wonSymbole.category,
+            symboles: [],
+            gift: wonSymbole.gift,
+            matched: false
+        };
+
+        // match the symbole
+        if (lotterySymbole.symboles[0] === wonSymbole.results[0]) {
+            match.symboles.push({ matched: true, symbole: lotterySymbole.symboles[0] });
+            match.matched = true;
+        }
+        else {
+            match.symboles.push({ matched: false, symbole: lotterySymbole.symboles[0] })
+        }
+
+        return match;
+    }
+
+    oneToManyMatch(wonSymbole: SpecialSymbole, lotterySymbole: LotterySpecialSymbole): MatchSpecialSymbole {
+        let match: MatchSpecialSymbole = {
+            category: wonSymbole.category,
+            symboles: [],
+            gift: wonSymbole.gift,
+            matched: false
+        };
+
+        // match the symboles
+        lotterySymbole.symboles.forEach((symbole, index) => {
+            if (symbole === wonSymbole.results[0]) {
+                match.symboles.push({ matched: true, symbole: symbole });
+                match.matched = true;
+            }
+            else {
+                match.symboles.push({ matched: false, symbole: symbole });
+            }
         })
 
-        return {
-            matchCount: matchCount,
-            totalCount: total,
-            matchStatus: matches
+        return match;
+    }
+
+    manyToOneMatch(wonSymbole: SpecialSymbole, lotterySymbole: LotterySpecialSymbole): MatchSpecialSymbole {
+        let match: MatchSpecialSymbole = {
+            category: wonSymbole.category,
+            symboles: [],
+            gift: wonSymbole.gift,
+            matched: false
+        };
+
+        // match the symboles
+        if (wonSymbole.results.includes(lotterySymbole.symboles[0])) {
+            match.symboles.push({ matched: true, symbole: lotterySymbole.symboles[0] });
+            match.matched = true;
         }
+        else {
+            match.symboles.push({ matched: false, symbole: lotterySymbole.symboles[0] })
+        }
+
+        return match
+    }
+
+    manyToManyMatch(wonSymbole: SpecialSymbole, lotterySymbole: LotterySpecialSymbole): MatchSpecialSymbole {
+        let match: MatchSpecialSymbole = {
+            category: wonSymbole.category,
+            symboles: [],
+            gift: wonSymbole.gift,
+            matched: false
+        };
+
+        // match the symboles
+        lotterySymbole.symboles.forEach((symbole, index) => {
+            if (wonSymbole.results.includes(symbole)) {
+                match.symboles.push({ matched: true, symbole: symbole });
+                match.matched = true;
+            }
+            else {
+                match.symboles.push({ matched: false, symbole: symbole });
+            }
+        })
+
+        return match
     }
 }
--- backend/test/business/strategies/ResultMatcher.test.ts
+++ backend/test/business/strategies/ResultMatcher.test.ts
@@ -1,5 +1,6 @@
 import { beforeEach, describe, test, expect, expectTypeOf } from "vitest";
 import ResultMatcher from "../../../src/business/strategies/ResultMatcher";
+import { LotterySpecialSymbole, MatchSpecialSymbole, SpecialSymbole } from "../../../src/business/types/types";
 
 describe("ResultMatcher", () => {
     let matcher: ResultMatcher;
@@ -9,17 +10,305 @@ describe("ResultMatcher", () => {
     })
 
 
-    test("should returns emtpty list if the input special symboles lists are emtpty ", () => {
+    test("should match in order and returns the status and match count", () => {
         // Arrange
-        const wonSpecialSymboles: [] = [];
-        const lotteryResultSymboles: [] = [];
+        const wonSymboles: string[] = ["4", "w", "4", "10", "20"];
+        const lotterySymboles: string[] = ["4", "w", "10", "4", "20"];
+        const wonList: boolean[] = [true, true, false, false, true];
 
         // Action
-        const result = matcher.checkSpecialSymboles(wonSpecialSymboles, lotteryResultSymboles);
+        const result = matcher.matchAllInOrderDescrete(wonSymboles, lotterySymboles);
 
         // Assert
-        expect(result).empty
+        expect(result.matchCount).toBe(3)
+        expect(result.totalCount).toBe(5)
+        expect(result.matchStatus.length).toBe(5)
+        result.matchStatus.forEach((item, index) => {
+            expect(item.symbole).toBe(lotterySymboles[index])
+            expect(item.matched).toBe(wonList[index])
+        })
     })
 
-    test("should ")
+    test("should match in any order and returns the status and match count correctly", () => {
+        // Arrange
+        const wonSymboles: string[] = ["4", "w", "4", "10", "20"];
+        const lotterySymboles: string[] = ["w", "10", "6", "11", "20"];
+        const wonList: boolean[] = [true, true, false, false, true];
+
+        // Action
+        const result = matcher.matchAllAnyOrderDescrete(wonSymboles, lotterySymboles);
+
+        // Assert
+        expect(result.matchCount).toBe(3)
+        expect(result.totalCount).toBe(5)
+        expect(result.matchStatus.length).toBe(5)
+        result.matchStatus.forEach((item, index) => {
+            expect(item.symbole).toBe(lotterySymboles[index])
+            expect(item.matched).toBe(wonList[index])
+        })
+    })
+
+    test("should check the special symboles in one to one match method and give the result", () => {
+        // Arrange
+        const wonSpecialSymboles: SpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                description: "test description",
+                method: "OneToOne",
+                results: ["65456"]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                description: "test description",
+                method: "OneToOne",
+                results: ["8564789"]
+            },
+        ]
+
+        const lotterySpecialSymboles1: LotterySpecialSymbole[] = [
+            {
+                category: "category 1",
+                symboles: ["65456"]
+            },
+            {
+                category: "category 2",
+                symboles: ["8564789"]
+            },
+            {
+                category: "category 1",
+                symboles: ["65488"]
+            },
+            {
+                category: "category 2",
+                symboles: ["65456"]
+            },
+        ]
+
+        const trueResult: MatchSpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                matched: true,
+                symboles: [
+                    { symbole: "65456", matched: true }
+                ]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                matched: true,
+                symboles: [
+                    { symbole: "8564789", matched: true }
+                ]
+            },
+            {
+                category: "category 1",
+                gift: "40000",
+                matched: false,
+                symboles: [
+                    { symbole: "65488", matched: false }
+                ]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                matched: false,
+                symboles: [
+                    { symbole: "65456", matched: false }
+                ]
+            },
+        ]
+
+        // Action
+        const symboleResult = matcher.checkSpecialSymboles(wonSpecialSymboles, lotterySpecialSymboles1)
+
+        // Assert
+        expect(symboleResult).toEqual(trueResult)
+        expect(symboleResult.length).toEqual(4)
+    })
+
+    test("should check the special symboles in one to many match method and give the result", () => {
+        // Arrange
+        const wonSpecialSymboles: SpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                description: "test description",
+                method: "OneToMany",
+                results: ["65456"]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                description: "test description",
+                method: "OneToMany",
+                results: ["8564789"]
+            },
+        ]
+
+        const lotterySpecialSymboles1: LotterySpecialSymbole[] = [
+            {
+                category: "category 1",
+                symboles: ["65456", "564773", "247475", "24204", "42474"]
+            },
+            {
+                category: "category 2",
+                symboles: ["12341234", "8564789", "23411", "5454641", "45484"]
+            }
+        ]
+
+        const trueResult: MatchSpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                matched: true,
+                symboles: [
+                    { symbole: "65456", matched: true },
+                    { symbole: "564773", matched: false },
+                    { symbole: "247475", matched: false },
+                    { symbole: "24204", matched: false },
+                    { symbole: "42474", matched: false },
+                ]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                matched: true,
+                symboles: [
+                    { symbole: "12341234", matched: false },
+                    { symbole: "8564789", matched: true },
+                    { symbole: "23411", matched: false },
+                    { symbole: "5454641", matched: false },
+                    { symbole: "45484", matched: false },
+                ]
+            },
+        ]
+
+        // Action
+        const symboleResult = matcher.checkSpecialSymboles(wonSpecialSymboles, lotterySpecialSymboles1)
+
+        // Assert
+        expect(symboleResult).toEqual(trueResult);
+    })
+
+    test("should check the special symboles in many to one match method and give the result", () => {
+        // Arrange
+        const wonSpecialSymboles: SpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                description: "test description",
+                method: "ManyToOne",
+                results: ["65456", "56474", "47278", "274758"]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                description: "test description",
+                method: "ManyToOne",
+                results: ["8564789", "78845", "9874656", "452548"]
+            },
+        ]
+
+        const lotterySpecialSymboles1: LotterySpecialSymbole[] = [
+            {
+                category: "category 1",
+                symboles: ["65456"]
+            },
+            {
+                category: "category 2",
+                symboles: ["8564785"]
+            }
+        ]
+
+        const trueResult: MatchSpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                matched: true,
+                symboles: [
+                    { symbole: "65456", matched: true },
+                ]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                matched: false,
+                symboles: [
+                    { symbole: "8564785", matched: false },
+                ]
+            },
+        ]
+
+        // Action
+        const symboleResult = matcher.checkSpecialSymboles(wonSpecialSymboles, lotterySpecialSymboles1)
+
+        // Assert
+        expect(symboleResult).toEqual(trueResult);
+    })
+
+
+    test("should check the special symboles in many to many match method and give the result", () => {
+        // Arrange
+        const wonSpecialSymboles: SpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                description: "test description",
+                method: "ManyToMany",
+                results: ["65456", "56474", "47278", "274758"]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                description: "test description",
+                method: "ManyToMany",
+                results: ["8564789", "78845", "9874656", "452548"]
+            },
+        ]
+
+        const lotterySpecialSymboles1: LotterySpecialSymbole[] = [
+            {
+                category: "category 1",
+                symboles: ["7845", "78745", "57586", "65456"]
+            },
+            {
+                category: "category 2",
+                symboles: ["78945", "75424", "8564799", "58686"]
+            }
+        ]
+
+        const trueResult: MatchSpecialSymbole[] = [
+            {
+                category: "category 1",
+                gift: "40000",
+                matched: true,
+                symboles: [
+                    { symbole: "7845", matched: false },
+                    { symbole: "78745", matched: false },
+                    { symbole: "57586", matched: false },
+                    { symbole: "65456", matched: true },
+                ]
+            },
+            {
+                category: "category 2",
+                gift: "car",
+                matched: false,
+                symboles: [
+                    { symbole: "78945", matched: false },
+                    { symbole: "75424", matched: false },
+                    { symbole: "8564799", matched: false },
+                    { symbole: "58686", matched: false },
+                ]
+            },
+        ]
+
+        // Action
+        const symboleResult = matcher.checkSpecialSymboles(wonSpecialSymboles, lotterySpecialSymboles1)
+
+        // Assert
+        expect(symboleResult).toEqual(trueResult);
+    })
 });
